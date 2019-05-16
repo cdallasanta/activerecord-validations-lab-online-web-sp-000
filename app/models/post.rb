5417c66938cf2ab00aaa3646d87c824ee8d481ad
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-16 21:12:51 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class TitleValidator < ActiveModel::EachValidator
     click_baits = [/Won't Believe/, /Secret/, /Guess/, /Top \d/]
     unless click_baits.any?{|regex| value =~ regex}
       binding.pry
-      record.errors(attribute) << (options[:message] || "is not clickbait-y enough")
+      record.errors[attribute] << (options[:message] || "is not clickbait-y enough")
     end
   end
 end
