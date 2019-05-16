da429c87a7cc3cb7f821e56d1b08a13628a4f0f7
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-16 21:02:37 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,7 @@ class TitleValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     click_baits = [/Won't Believe/, /Secret/, /Guess/, /Top \d/]
     unless click_baits.any?{|regex| value =~ regex}
-      record.errors(attribute) << (options[:message] || "is not an email")
+      record.errors(attribute) << (options[:message] || "is not clickbait-y enough")
+    end
+  end
+end
