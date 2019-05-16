f8b0ff7bd31e4611732014b7cae2f583ff25e320
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-16 21:13:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class TitleValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     click_baits = [/Won't Believe/, /Secret/, /Guess/, /Top \d/]
     unless click_baits.any?{|regex| value =~ regex}
-      binding.pry
       record.errors[attribute] << (options[:message] || "is not clickbait-y enough")
     end
   end
