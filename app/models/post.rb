3077be4ff626804e787303ad3b0b59cd971b6a2c
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-16 21:10:36 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,3 @@
-class Post < ActiveRecord::Base
-  validates :title, presence: true, title: true
-  validates :content, length: {minimum: 250}
-  validates :summary, length: {maximum: 250}
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-end
-
 class TitleValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     click_baits = [/Won't Believe/, /Secret/, /Guess/, /Top \d/]
@@ -12,4 +5,11 @@ class TitleValidator < ActiveModel::EachValidator
       record.errors(attribute) << (options[:message] || "is not clickbait-y enough")
     end
   end
+end
+
+class Post < ActiveRecord::Base
+  validates :title, presence: true, title: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
